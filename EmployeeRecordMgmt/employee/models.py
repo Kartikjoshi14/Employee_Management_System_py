2cf152cabf58a4588d9510cfc1be00b1e8b03fdb
Kartik Joshi <14kartikjoshi@gmail.com>
2025-01-30 01:32:22 +0530

    day 2: created functionality to input data and made migrations to add the given information

--- EmployeeRecordMgmt/employee/models.py
+++ EmployeeRecordMgmt/employee/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+#whatever data we fetch gets stored here
+#whener we make changes in models.py we have to write make migrations
+class EmployeeDetail(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    empcode = models.CharField(max_length=50)
+    empdept = models.CharField(max_length=100,null=True)
+    designation = models.CharField(max_length=100,null=True)
+    contact = models.CharField(max_length=15,null=True)
+    gender = models.CharField(max_length=50,null=True)
+    joiningdate = models.DateField(null=True)
+    def __str__(self):
+        return self.user.username
