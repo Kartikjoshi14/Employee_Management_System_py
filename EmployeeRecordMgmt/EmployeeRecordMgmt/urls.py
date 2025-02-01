fb3c3380a4506346abba5f8581aa01ba3cfd1cc5
Kartik Joshi <14kartikjoshi@gmail.com>
2025-02-02 01:07:03 +0530

    Day 5: developed the profile page and added functions to emp_login page.fixed a number of bugs

--- EmployeeRecordMgmt/EmployeeRecordMgmt/urls.py
+++ EmployeeRecordMgmt/EmployeeRecordMgmt/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('registration', registration,name = 'registration'),
     path('emp_login', emp_login,name = 'emp_login'),
     path('emp_home', emp_home,name = 'emp_home'),
+    path('profile', profile,name = 'profile'),
 
 ]
